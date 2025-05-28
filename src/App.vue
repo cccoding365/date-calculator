f45a4d8bc0050ede9c86ed79895fc5a6d2bb5a52
DoubledConG <992196871@qq.com>
2025-05-28 20:35:29 +0800

    创建我的Vue项目启动模版工程

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center items-center gap-10">
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="size-20" alt="Vite logo" />
+    </a>
+    <a href="https://tailwindcss.com/" target="_blank">
+      <img src="./assets/tailwindcss.svg" class="size-30" alt="TailwindCSS logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="size-20" alt="Vue logo" />
+    </a>
+  </div>
+  <h1 class="text-4xl font-semibold leading-20">Vite + Vue + TailwindCSS</h1>
+  <h3 class="text-2xl leading-10 text-gray-300">My Project Starter</h3>
+</template>
